e431a581db4ece7bdb23c60917a10cd78710eb3d
mikol <mikolajsadownik@outlook.com>
2026-04-29 19:27:19 +0200

    Created resource group

---
+++ Terraform/main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "RG-Azure-Data-Flow" {
+  name     = "RG-Azure-Data-Flow"
+  location = "West Europe"
+}
---
+++ Terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the Azure Resource Group"
+}
+
+variable "region" {
+  type        = string
+  description = "Azure region"
+}
